0df6b83a49976f15b03a7ec3a25cf17bed8b5574
kevinnadr <kevin19305.ib@gmail.com>
2024-05-07 17:41:24 +0700

    masukan int main

--- tugas1.cpp
+++ tugas1.cpp
@@ -3,4 +3,24 @@ using namespace std;
 
 class mahasiswa{
     public:
-    
+    int nim;
+    void showNim(){
+        cout<< "No Induk = "<<nim<<endl;
+    }
+};
+
+int main (){
+
+    mahasiswa mhs{1}; // object mhs
+    mhs.showNim(); // member acces operator
+
+    mahasiswa &refMhs = mhs;
+    refMhs.nim = 2; // member access operator
+    mhs.showNim();
+
+    mahasiswa *pMhs = &mhs;
+    pMhs->nim = 3;
+    pMhs->showNim();
+    return 0;
+
+}
